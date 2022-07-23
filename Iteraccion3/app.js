1a6f97e20fad2ec108b42ffe35ed9b4e5c3196fd
Kroquetitaa <Kroquetita@gmail.com>
2022-07-23 18:49:10 +0200

    exercise 3.5

--- Iteraccion3/app.js
+++ Iteraccion3/app.js
@@ -34,3 +34,13 @@ const toyUpdate = {lights: 'rgb', power: ['Volar like a dragon', 'MoonWalk']}
 
 const copyToys = {...toy1, ...toyUpdate};
 console.log( copyToys );
+
+// 3.5 Dado el siguiente array. Crear una copia de él eliminando la posición 2 
+// pero sin editar el array inicial. De nuevo, usando spread operatos.
+// const colors = ['rojo', 'azul', 'amarillo', 'verde', 'naranja'];
+
+const colors = ['rojo', 'azul', 'amarillo', 'verde', 'naranja'];
+
+const copyColors = [...colors];
+const removeItem = copyColors.splice(2,1);
+console.log( copyColors  );
