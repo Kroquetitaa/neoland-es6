fc1efc56b71bf5ba77c0b4744e049c7af0837192
Kroquetitaa <Kroquetita@gmail.com>
2022-07-23 20:25:09 +0200

    exercise 5.1

---
+++ Iteraccion5/app.js
@@ -0,0 +1,7 @@
+// 5.1 Dado el siguiente array, utiliza .filter() para generar un nuevo array 
+// con los valores que sean mayor que 18
+// const ages = [22, 14, 24, 55, 65, 21, 12, 13, 90];
+
+const ages = [22, 14, 24, 55, 65, 21, 12, 13, 90];
+
+console.log( ages.filter( numbers => numbers >= 18) )
