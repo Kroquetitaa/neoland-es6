fc4f95c07f7e15d5f7a1786a97fde02656c767a1
Kroquetitaa <Kroquetita@gmail.com>
2022-07-23 17:59:33 +0200

    exercise 2.2

--- Iteraccion2/app.js
+++ Iteraccion2/app.js
@@ -7,3 +7,13 @@ const game = {title: 'The last us 2', gender: ['action', 'zombie', 'survival'], 
 
 const { title, gender, year } = game;
 console.log( title, gender, year );
+
+// 2.2 En base al siguiente javascript, usa destructuring para crear 3 variables 
+// llamadas fruit1, fruit2 y fruit3, con los valores del array. Posteriormente
+// imprimelo por consola.
+// const fruits = ['Banana', 'Strawberry', 'Orange'];
+
+const fruits = ['Banana', 'Strawberry', 'Orange'];
+
+const [fruit1, fruit2, fruit3 ] = fruits;
+console.log( fruits );
