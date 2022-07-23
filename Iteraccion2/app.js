f729392dc1a5c3dd9230afdede2ca872c28282e9
Kroquetitaa <Kroquetita@gmail.com>
2022-07-23 18:24:13 +0200

    exercise 2.4

--- Iteraccion2/app.js
+++ Iteraccion2/app.js
@@ -30,3 +30,16 @@ const animalFunction = ( {} ) => {
 };
 
 console.log( animalFunction({}) );
+
+// 2.4 En base al siguiente javascript, usa destructuring para crear las 
+// variables name y itv con sus respectivos valores. Posteriormente crea 
+// 3 variables usando igualmente el destructuring para cada uno de los años 
+// y comprueba que todo esta bien imprimiendolo.
+
+// const car = {name: 'Mazda 6', itv: [2015, 2011, 2020] }
+
+const car = {name: 'Mazda 6', itv: [2015, 2011, 2020] };
+const { name, itv } = car;
+console.log( car );
+const [ yearOne, yearTwo, yearThree ] = itv;
+console.log( yearOne, yearTwo, yearThree );
