3b7bf6df73f8d953020592ce8957118700e8d2cf
Kroquetitaa <Kroquetita@gmail.com>
2022-07-23 18:28:10 +0200

    exercise 3.2

--- Iteraccion3/app.js
+++ Iteraccion3/app.js
@@ -5,3 +5,10 @@ const pointsList = [32, 54, 21, 64, 75, 43];
 
 const copiaPointsList = [...pointsList];
 console.log( copiaPointsList );
+
+// 3.2 Dado el siguiente objeto, crea una copia usando spread operators.
+// const toy = {name: 'Bus laiyiar', date: '20-30-1995', color: 'multicolor'};
+
+const toy = {name: 'Bus laiyiar', date: '20-30-1995', color: 'multicolor'};
+const copyToy = {...toy};
+console.log( copyToy );
