9a5d28bbf65f8d2ad1509f6f1f79e858af185e8b
Kroquetitaa <Kroquetita@gmail.com>
2022-07-23 20:26:15 +0200

    exercise 5.2

--- Iteraccion5/app.js
+++ Iteraccion5/app.js
@@ -5,3 +5,11 @@
 const ages = [22, 14, 24, 55, 65, 21, 12, 13, 90];
 
 console.log( ages.filter( numbers => numbers >= 18) )
+
+// 5.2 Dado el siguiente array, utiliza .filter() para generar un nuevo array 
+// con los valores que sean par.
+// const ages = [22, 14, 24, 55, 65, 21, 12, 13, 90];
+
+const ages2 = [22, 14, 24, 55, 65, 21, 12, 13, 90];
+
+console.log( ages2.filter( numbers => (numbers % 2 == 0)) )
