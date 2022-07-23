618eb51f012edc65e4cd92026c886692748eb73f
Kroquetitaa <Kroquetita@gmail.com>
2022-07-23 18:26:33 +0200

    exercise 3.1

---
+++ Iteraccion3/app.js
@@ -0,0 +1,7 @@
+// 3.1 Dado el siguiente array, crea una copia usando spread operators.
+// const pointsList = [32, 54, 21, 64, 75, 43]
+
+const pointsList = [32, 54, 21, 64, 75, 43];
+
+const copiaPointsList = [...pointsList];
+console.log( copiaPointsList );
