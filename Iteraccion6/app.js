a4bfec38e50394622507c15646f8d58cc541fdc1
Kroquetitaa <Kroquetita@gmail.com>
2022-07-24 18:39:49 +0200

    exercise 6.1

---
+++ Iteraccion6/app.js
@@ -0,0 +1,6 @@
+// 6.1 Dado el siguiente array, usa .find() para econtrar el número 100.
+// const numbers = [32, 21, 63, 95, 100, 67, 43];
+
+const numbers = [32, 21, 63, 95, 100, 67, 43];
+
+console.log( numbers.find( e => e === 100))
