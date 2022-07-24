85b5da9e91a4f8772b9e13c3fed4a19765903261
Kroquetitaa <Kroquetita@gmail.com>
2022-07-24 18:24:12 +0200

    exercise 5.6

---
+++ Iteraccion5/iteraccion5.6/app.js
@@ -0,0 +1,26 @@
+// 5.6 Dado el siguiente html y javascript, utiliza .filter() para mostrar por consola 
+// los streamers que incluyan la palabra introducida en el input. De esta forma, si 
+// introduzco 'Ru' me deberia de mostrar solo el streamer 'Rubius'. Si
+// introduzco 'i', me deberia de mostrar el streamer 'Rubius' e 'Ibai'.
+// const streamers = [
+// 	{name: 'Rubius', age: 32, gameMorePlayed: 'Minecraft'},
+// 	{name: 'Ibai', age: 25, gameMorePlayed: 'League of Legends'},
+// 	{name: 'Reven', age: 43, gameMorePlayed: 'League of Legends'},
+// 	{name: 'AuronPlay', age: 33, gameMorePlayed: 'Among Us'}
+// ];
+
+let input = document.querySelector('input');
+console.log( input );
+
+const streamers = [
+	{name: 'Rubius', age: 32, gameMorePlayed: 'Minecraft'},
+	{name: 'Ibai', age: 25, gameMorePlayed: 'League of Legends'},
+	{name: 'Reven', age: 43, gameMorePlayed: 'League of Legends'},
+	{name: 'AuronPlay', age: 33, gameMorePlayed: 'Among Us'}
+];
+
+console.log( streamers );
+
+const searchWords = () => {
+	console.log( streamers.filter( value => (value.name.includes( input.value )) ? value.name : '' ) );
+}
