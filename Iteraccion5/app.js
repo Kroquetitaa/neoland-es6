c1743908afebf3ac3c936cc85c9dcd4923157af1
Kroquetitaa <Kroquetita@gmail.com>
2022-07-24 18:10:01 +0200

    exercise 5.5

--- Iteraccion5/app.js
+++ Iteraccion5/app.js
@@ -50,3 +50,21 @@ const streamers2 = [
 ];
 
 console.log( streamers2.filter( value => value.name.includes('u')))
+
+// 5.5 utiliza .filter() para generar un nuevo array con los streamers que incluyan 
+// el caracter 'Legends' en su propiedad .gameMorePlayed. Recomendamos usar la funcion 
+// .includes() para la comprobación.
+// Además, pon el valor de la propiedad .gameMorePlayed a MAYUSCULAS cuando 
+// .age sea mayor que 35.
+
+console.log('----------------------');
+
+const streamers3 = [
+	{name: 'Rubius', age: 32, gameMorePlayed: 'Minecraft'},
+	{name: 'Ibai', age: 25, gameMorePlayed: 'League of Legends'},
+	{name: 'Reven', age: 43, gameMorePlayed: 'League of Legends'},
+	{name: 'AuronPlay', age: 33, gameMorePlayed: 'Among Us'}
+];
+
+const copystreamers3 = streamers3.filter( value => value.gameMorePlayed.includes('Legends') ).map( value => (value.age > 35) ? value.gameMorePlayed.toUpperCase() : value.gameMorePlayed.toLowerCase() )
+console.log( copystreamers3 );
