b6c4c38be17545a9acf2378516f29e857eaea0d7
Kroquetitaa <Kroquetita@gmail.com>
2022-07-24 19:10:24 +0200

    exercise 6.3

--- Iteraccion6/app.js
+++ Iteraccion6/app.js
@@ -19,3 +19,33 @@ const movies = [
 ];
 
 console.log( movies.find( value => value.date === 2010) );
+
+// 6.3 Dado el siguiente javascript, usa .find() para econtrar el alien de nombre 
+// 'Cucushumushu' y la mutación 'Porompompero'. Una vez que los encuentres, usa 
+// spread operator para fusionarlos teniendo en cuenta que el objeto de la mutación 
+// lo queremos meter en la propiedad .mutation del objeto fusionado.
+// const aliens = [
+// 	{name: 'Zalamero', planet: 'Eden', age: 4029},
+// 	{name: 'Paktu', planet: 'Andromeda', age: 32},
+// 	{name: 'Cucushumushu', planet: 'Marte', age: 503021}
+// ];
+// const mutations = [
+// 	{name: 'Porompompero', description: 'Hace que el alien pueda adquirir la habilidad de tocar el tambor'},
+// 	{name: 'Fly me to the moon', description: 'Permite volar, solo y exclusivamente a la luna'},
+// 	{name: 'Andando que es gerundio', description: 'Invoca a un señor mayor como Personal Trainer'}
+// ];
+const aliens = [
+	{name: 'Zalamero', planet: 'Eden', age: 4029},
+	{name: 'Paktu', planet: 'Andromeda', age: 32},
+	{name: 'Cucushumushu', planet: 'Marte', age: 503021}
+];
+const mutations = [
+	{name: 'Porompompero', description: 'Hace que el alien pueda adquirir la habilidad de tocar el tambor'},
+	{name: 'Fly me to the moon', description: 'Permite volar, solo y exclusivamente a la luna'},
+	{name: 'Andando que es gerundio', description: 'Invoca a un señor mayor como Personal Trainer'}
+];
+
+const valueOne = aliens.find( value => value.name === 'Cucushumushu');
+const valueTwo = mutations.find( value => value.name === 'Porompompero');
+const newObject = {...valueOne, valueTwo};
+console.log( newObject );
