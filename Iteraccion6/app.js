510b979dea730cca7aabde65566da564e6ffd70b
Kroquetitaa <Kroquetita@gmail.com>
2022-07-24 19:05:14 +0200

    exercise 6.2

--- Iteraccion6/app.js
+++ Iteraccion6/app.js
@@ -3,4 +3,19 @@
 
 const numbers = [32, 21, 63, 95, 100, 67, 43];
 
-console.log( numbers.find( e => e === 100))
+console.log( numbers.find( e => e === 100));
+
+// 6.2 Dado el siguiente array, usa .find() para econtrar la pelicula del año 2010.
+// const movies = [
+// 	{title: 'Madagascar', stars: 4.5, date: 2015},
+// 	{title: 'Origen', stars: 5, date: 2010},
+// 	{title: 'Your Name', stars: 5, date: 2016}
+// ];
+
+const movies = [
+	{title: 'Madagascar', stars: 4.5, date: 2015},
+	{title: 'Origen', stars: 5, date: 2010},
+	{title: 'Your Name', stars: 5, date: 2016}
+];
+
+console.log( movies.find( value => value.date === 2010) );
